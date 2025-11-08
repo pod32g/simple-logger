9aefeec97c92930142dd36731c4edf819c6760cd
pod32g <3311662+pod32g@users.noreply.github.com>
2025-11-08 02:35:35 -0600

    Refresh examples for new logging features

--- example/custom_args/main.go
+++ example/custom_args/main.go
@@ -15,8 +15,10 @@ func main() {
 	cfg.Custom = &SimpleStreamingFormatter{}
 
 	logger := log.ApplyConfig(cfg)
+	defer logger.Close()
 
 	logger.Info("User", 7, "performed action")
+	logger.InfoFields("User fields", log.Int("user_id", 7), log.String("action", "performed"))
 }
 
 // SimpleStreamingFormatter implements ArgsFormatter for demonstration
@@ -28,15 +30,25 @@ func (f *SimpleStreamingFormatter) Format(level log.LogLevel, message string) st
 	return b.String()
 }
 
-func (f *SimpleStreamingFormatter) FormatArgs(level log.LogLevel, w io.Writer, v ...interface{}) {
-	fmt.Fprintf(w, "%s [%s] ", time.Now().Format("2006-01-02 15:04:05"), levelToString(level))
-	for i, val := range v {
-		if i > 0 {
-			fmt.Fprint(w, " ")
-		}
-		fmt.Fprint(w, val)
+func (f *SimpleStreamingFormatter) FormatWithFields(level log.LogLevel, message string, fields []log.Field) string {
+	var b strings.Builder
+	f.FormatArgsWithFields(level, fields, &b, message)
+	return b.String()
+}
+
+func (f *SimpleStreamingFormatter) FormatArgsWithFields(level log.LogLevel, fields []log.Field, w io.Writer, v ...interface{}) {
+	fmt.Fprintf(w, "%s [%s]", time.Now().Format("2006-01-02 15:04:05"), levelToString(level))
+	for _, val := range v {
+		fmt.Fprintf(w, " %v", val)
+	}
+	for _, field := range fields {
+		fmt.Fprintf(w, " %s=%v", field.Key, field.Value)
 	}
 	fmt.Fprint(w, "\n")
+}
+
+func (f *SimpleStreamingFormatter) FormatArgs(level log.LogLevel, w io.Writer, v ...interface{}) {
+	f.FormatArgsWithFields(level, nil, w, v...)
 }
 
 func levelToString(level log.LogLevel) string {
